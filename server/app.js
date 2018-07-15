8df007ffc2fe08e1baf8de14ea7aa804ccf4d185
georginio <ggola93@gmail.com>
2018-07-15 15:06:47 +0400

    production build

--- server/app.js
+++ server/app.js
@@ -33,6 +33,15 @@ app.delete('/api/users/all', (req, res) => {
     UserList.removeUsers();
 });
 
-app.get('*', (req, res) => res.send('not found'));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+    });
+ } else {
+     app.get('*', (req, res) => res.send('not found'));
+ }
+
 
 initIO(io);
