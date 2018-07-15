5e0a320550da701bd3ef1076c95153253d67885d
georginio <ggola93@gmail.com>
2018-07-15 15:38:03 +0400

    resolve path from root

--- server/app.js
+++ server/app.js
@@ -34,10 +34,10 @@ app.delete('/api/users/all', (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('/client/build'));
     const path = require('path');
+    app.use(express.static(path.resolve(__dirname, '../client/build')));
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
     });
  } else {
      app.get('*', (req, res) => res.send('not found'));
